a25f538887fb7138e6d2bb2c2fa53688049ff04f
rafreza <rafintokyo@gmail.com>
2021-04-27 21:48:44 -0400

    Add header, style app color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Header.css'
 import { Navbar, Nav } from 'react-bootstrap'
 
 function Header(props) {
   return (
     <div className="wrapper">
-      <h1>Shoppies</h1>
+      <h1 className="header-text">The Shoppies</h1>
     </div>
   );
 }
